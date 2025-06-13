b999871bd107d136cd0514109d16f95801d29edc
Lexy Perry <lexy.perry61022@gmail.com>
2025-06-12 22:30:38 -0400

    created an app that stores data in local storage. On page refresh storage persists.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,21 @@
+import { use, useEffect, useState } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 function Form() {
-
+const [name,setName]=useState(()=> localStorage.getItem("name") || "");
+const [service,setService]=useState(()=> localStorage.getItem('service') || "");
+useEffect(()=>{
+  localStorage.setItem("name",name);
+},["name" ,name]); 
+  useEffect(()=>{
+    localStorage.setItem("service", service);
+  },["service", service]);
     return (
       <>
         <form style={{ display: "flex", flexDirection: "column" }}>
             <label htmlFor="name">Name:</label>
-            <input type="text" data-testid={"name"} />
+            <input type="text" data-testid={"name"} onChange={(e)=> setName(e.target.value)}/>
             <label htmlFor="service">Service Number:</label>
-            <input type="text" data-testid={"service"} />
+            <input type="text" data-testid={"service"} onChange={(e)=> setService(e.target.value)}/>
 
         </form>
         <h4>{name ? `Welcome, ${name}!` : "Enter your name"}</h4>
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,22 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const [value , setValue]=useState(()=>{
+        try{
+            const item=window.localStorage.getItem(key);
+            return item ? JSON.parse(item) :initialValue;
+        } catch (error){
+            return initialValue
+        }
+    });
+    const setData = (data)=>{
+        try{
+            const dataToStore= data instanceof Function ? value(value):data;
+            setStoredData(dataToStore);
+            window.localStorage.setItem(key,JSON.stringify(dataToStore));
+    }catch (error){
+        console.log(error);
+    }
+    }
+    return [value,setData];
 }
